186252a875fd2b7c0d8a459c7f4ce9d1f14186d6
tienraver <tienraver@gmail.com>
2025-10-15 06:29:26 +0700

    xxx

--- example.py
+++ example.py
@@ -3,9 +3,26 @@ from tensorflow import keras
 
 # CNN
 inputs = keras.layers.Input(shape=[32,32,3])
-x1 = keras.layers.Conv2D(filters=32,kernel_size=(3,3),padding="same",activation="relu")(inputs) # L1
-x1 = keras.layers.Conv2D(filters=32,kernel_size=(3,3),padding="same",activation="relu")(x1) # L2
-prediction = keras.layers.MaxPool2D(pool_size=(2,2))(x1) # Downsize
+x1 = keras.layers.Conv2D(filters=32,kernel_size=(3,3),padding="same",activation="relu")(inputs)
+x1 = keras.layers.Conv2D(filters=32,kernel_size=(3,3),padding="same",activation="relu")(x1)
+prediction = keras.layers.MaxPool2D(pool_size=(2,2))(x1)
 model = keras.models.Model(inputs=inputs,outputs=prediction,name="A.I LE")
 
 model.summary()
+
+
+"""
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+
+model_v1 = keras.Sequential([
+    keras.layers.Dense(1024,input_dim=64),
+    keras.layers.Activation("relu"),
+    keras.layers.Dense(256),
+    keras.layers.Activation("softmax")
+])
+model_v1.summary()
+model_v1.inputs
+model_v1.outputs
+"""
