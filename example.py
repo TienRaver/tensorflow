c709bf58159b2cb52b12465473450e868ec40d9d
tienraver <tienraver@gmail.com>
2025-10-18 22:36:39 +0700

    xxx

--- example.py
+++ example.py
@@ -1,19 +1,42 @@
-# Cach 1: Tao layer truc tiep trong Sequential
+
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 
-model_v1 = keras.Sequential([
-    keras.layers.Dense(1024,input_dim=64),
-    keras.layers.Activation("relu"),
-    keras.layers.Dense(256),
-    keras.layers.Activation("softmax")])
-model_v1.summary()
+# Xay dung model
+model = keras.models.Model(
+    keras.layers.Input(shape=[])
+)
 
-# Cach 2: Tao Sequential roi gan tung layer vao
-model_v2 = keras.Sequential()
-model_v2.add(keras.layers.Dense(1024,input_dim=64))
-model_v2.add(keras.layers.Activation("relu"))
-model_v2.add(keras.layers.Dense(256))
-model_v2.add(keras.layers.Activation("softmax"))
-model_v2.summary()
+"""
+# Tạo dữ liệu giả: 2000 mẫu, mỗi mẫu có 64 đặc trưng
+data = np.random.random((2000,64))
+labels = np.random.randint(0,10,(2000,))
+labels = tf.keras.utils.to_categorical(labels, num_classes=10)
+
+valid_data = np.random.random((500,64))
+valid_labels = tf.keras.utils.to_categorical(np.random.randint(0,10,(500,)), num_classes=10)
+
+test_data = np.random.random((500,64))
+test_labels = tf.keras.utils.to_categorical(np.random.randint(0,10,(500,)), num_classes=10)
+
+# Xây dựng model
+model_v1 = keras.Sequential([
+    keras.Input(shape=(64,)),
+    keras.layers.Dense(1024, activation="relu"),
+    keras.layers.Dense(256, activation="relu"),
+    keras.layers.Dense(10, activation="softmax")
+])
+
+model_v1.compile(
+    optimizer="adam",
+    loss="categorical_crossentropy",
+    metrics=["accuracy"]
+)
+
+# Huấn luyện
+model_v1.fit(data, labels, epochs=10, batch_size=50, validation_data=(valid_data, valid_labels))
+
+# Đánh giá
+model_v1.evaluate(test_data, test_labels)
+"""
