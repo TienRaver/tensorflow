6729a1c362679440a88dcb2ccb3e33d2ff283355
tienraver <tienraver@gmail.com>
2025-10-22 20:30:42 +0700

    xx

--- example.py
+++ example.py
@@ -4,8 +4,5 @@ import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 
-arr = np.random.randint(0,256,(500,500))
-image = Image.fromarray(arr)
-plt.figure(num=10,figsize=(8,6))
-plt.imshow(image,cmap="gray")
-plt.show()
+arr = np.random.random(0,256,(50000,32,32,1))
+print(arr)
